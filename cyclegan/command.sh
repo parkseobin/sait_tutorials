3dbdb6bce888cbf226ab93e525e04cf8bcb1adb1
seobin <kor.p.sb@gmail.com>
2019-12-24 17:07:32 +0900

    .

---
+++ cyclegan/command.sh
@@ -0,0 +1,2 @@
+#python main.py --dataset_dir=horse2zebra --continue_train=True --lr 0.
+python main.py --dataset_dir=horse2zebra 
---
+++ vae/command.sh
@@ -0,0 +1,5 @@
+python run_main.py --dim_z 2 --num_epochs 60 --PMLR True
+
+
+### denoising with VAE
+#python run_main.py --dim_z 20 --add_noise True --num_epochs 40
